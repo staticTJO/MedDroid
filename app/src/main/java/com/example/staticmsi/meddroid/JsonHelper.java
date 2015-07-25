eb8f956a4c1bb4b04e356446a552b8dd245cec16
Hamad Almarri <hamadmarri@yahoo.com>
2015-07-25 02:39:36 -0600

    two json models Patient and PatientReport are done

---
+++ app/src/main/java/com/example/staticmsi/meddroid/JsonHelper.java
@@ -0,0 +1,218 @@
+package com.example.staticmsi.meddroid;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.protocol.HttpContext;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Created by hamadalmarri on 2015-07-22.
+ */
+public class JsonHelper {
+
+
+    private static String domain = "http://10.0.2.2:8080/MyPatients_Spring";
+
+
+    public static String GET(String path) {
+        Requesting rq = new Requesting();
+
+        rq.execute("get", domain + path);
+
+        while (rq.result == null) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return rq.result;
+    }
+
+    public static String PUT(String path, String json) {
+        Requesting rq = new Requesting();
+
+        rq.execute("put", domain + path, json);
+
+        return rq.result;
+    }
+
+    public static String POST(String path, String json) {
+        Requesting rq = new Requesting();
+
+        rq.execute("post", domain + path, json);
+
+        return rq.result;
+    }
+
+    public static String DELETE(String path) {
+        Requesting rq = new Requesting();
+
+        rq.execute("delete", domain + path);
+
+        return rq.result;
+    }
+
+    private static class Requesting extends AsyncTask<String, Void, String> {
+
+        public static String result = null;
+
+        @Override
+        protected String doInBackground(String... urls) {
+            if (urls[0].equals("get"))
+                return GET(urls[1]);
+            else if (urls[0].equals("put"))
+                return PUT(urls[1], urls[2]);
+            else if (urls[0].equals("post"))
+                return POST(urls[1], urls[2]);
+            else if (urls[0].equals("delete"))
+                return DELETE(urls[1]);
+
+            return "-1";
+        }
+
+        public static String DELETE(String url) {
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpContext localContext = new BasicHttpContext();
+            HttpDelete delete = new HttpDelete(url);
+
+            delete.addHeader("Accept", "application/json");
+
+            try {
+                HttpResponse response = httpClient.execute(delete, localContext);
+//                HttpEntity entity = response.getEntity();
+            } catch (Exception e) {
+                return e.getLocalizedMessage();
+            }
+
+            result = "done";
+            return result;
+        }
+
+
+        public static String POST(String url, String json) {
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpContext localContext = new BasicHttpContext();
+            HttpPost post = new HttpPost(url);
+            StringEntity se = null;
+
+            try {
+                se = new StringEntity(json);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+
+            se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+
+            post.addHeader("Accept", "application/json");
+            post.addHeader("Content-type", "application/json");
+            post.setEntity(se);
+
+
+            try {
+                HttpResponse response = httpClient.execute(post, localContext);
+                HttpEntity entity = response.getEntity();
+            } catch (Exception e) {
+                return e.getLocalizedMessage();
+            }
+
+            result = "done";
+            return result;
+        }
+
+
+        public static String PUT(String url, String json) {
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpContext localContext = new BasicHttpContext();
+            HttpPut put = new HttpPut(url);
+            StringEntity se = null;
+
+            try {
+                se = new StringEntity(json);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+
+            se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+
+            put.addHeader("Accept", "application/json");
+            put.addHeader("Content-type", "application/json");
+            put.setEntity(se);
+
+
+            try {
+                HttpResponse response = httpClient.execute(put, localContext);
+                HttpEntity entity = response.getEntity();
+//                result = getASCIIContentFromEntity(entity);
+            } catch (Exception e) {
+                return e.getLocalizedMessage();
+            }
+
+            result = "done";
+            return result;
+        }
+
+        public static String GET(String url) {
+
+            InputStream inputStream = null;
+            String result = "";
+
+            try {
+
+                // create HttpClient
+                HttpClient httpclient = new DefaultHttpClient();
+
+                // make GET request to the given URL
+                HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+
+                // receive response as inputStream
+                inputStream = httpResponse.getEntity().getContent();
+
+                // convert inputstream to string
+                if (inputStream != null)
+                    result = convertInputStreamToString(inputStream);
+                else
+                    result = "-1";
+
+            } catch (Exception e) {
+                Log.d("InputStream", e.getLocalizedMessage());
+            }
+
+            Requesting.result = result;
+            return result;
+        }
+
+        private static String convertInputStreamToString(InputStream inputStream) throws IOException {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            StringBuilder result = new StringBuilder();
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null)
+                result.append(line);
+
+            inputStream.close();
+            return result.toString();
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/staticmsi/meddroid/models/PatientReport.java
@@ -0,0 +1,146 @@
+package com.example.staticmsi.meddroid.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by hamadalmarri on 2015-07-23.
+ */
+public class PatientReport extends AbstractJsonModel {
+
+    private Long id = null;
+    private String reportName;
+    private Patient patientFile = null;
+    private Long version = 0L;
+
+
+    public String getReportName() {
+        return reportName;
+    }
+
+    public void setReportName(String reportName) {
+        this.reportName = reportName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Patient getPatientFile() {
+        return patientFile;
+    }
+
+    public void setPatientFile(Patient patientFile) {
+        this.patientFile = patientFile;
+    }
+
+    public Long getVersion() {
+        return version;
+    }
+
+    @Override
+    public String toString() {
+        return "PatientReport{" +
+                "id=" + id +
+                ", reportName='" + reportName + '\'' +
+                ", patient{" + patientFile.toString() + "}" +
+                ", version=" + version +
+                '}';
+    }
+
+    @Override
+    protected Long getModelId() {
+        return id;
+    }
+
+    @Override
+    protected String getPath() {
+        return "/patientreports/";
+    }
+
+
+    public static Patient fromJson(String json) {
+        JSONObject jsonObject = null;
+        try {
+            jsonObject = new JSONObject(json);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return (Patient) new Patient().fromJson(jsonObject);
+    }
+
+    @Override
+    public AbstractJsonModel fromJson(JSONObject jsonObject) {
+        PatientReport p = new PatientReport();
+
+        try {
+            if (jsonObject.has("id"))
+                p.id = jsonObject.getLong("id");
+
+            p.setReportName(jsonObject.getString("reportName"));
+
+            if (jsonObject.has("patientFile"))
+                p.patientFile = Patient.fromJson(jsonObject.getString("patientFile"));
+
+            if (jsonObject.has("version"))
+                p.version = jsonObject.getLong("version");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return p;
+    }
+
+    @Override
+    public String toJson(boolean addVersion) {
+        JSONObject jsonObject = new JSONObject();
+
+        try {
+
+            jsonObject.put("createdBy", JSONObject.NULL);
+
+
+            if (id != null)
+                jsonObject.put("id", this.id);
+
+
+            if (patientFile != null)
+                jsonObject.put("patientFile", new JSONObject(patientFile.toJson(true)));
+
+            jsonObject.put("reportName", this.reportName);
+
+            if (addVersion)
+                jsonObject.put("version", this.version);
+
+            return jsonObject.toString();
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static PatientReport findById(Long id) {
+
+        return (PatientReport) new PatientReport().superFindById(id);
+    }
+
+
+    public static List<PatientReport> findAll() {
+
+        List<AbstractJsonModel> abstractJsonModels = new PatientReport().superFindAll();
+        List<PatientReport> patientReports = new ArrayList<PatientReport>();
+
+        for (AbstractJsonModel jm : abstractJsonModels)
+            patientReports.add((PatientReport) jm);
+
+        return patientReports;
+    }
+
+}
