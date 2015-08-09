2a251a957c4835aeacc67856d48c09da0c3e0cc0
StaticTJO <sk8terboy5931@hotmail.com>
2015-08-09 05:10:22 -0600

    Fixed up NeuroActivity Button

--- app/src/main/java/com/example/staticmsi/meddroid/RespActivity.java
+++ app/src/main/java/com/example/staticmsi/meddroid/RespActivity.java
@@ -127,7 +127,6 @@ public class RespActivity extends Activity {
 
 
 
-
     private void setEventListeners() {
         Button btnToCardio = (Button) findViewById(R.id.btntocardio);
         Button btnCancel = (Button) findViewById(R.id.btncancelres);
@@ -164,48 +163,17 @@ public class RespActivity extends Activity {
                 CheckBox RespiratoryRespirationsBronchial = (CheckBox) findViewById(R.id.RespiratoryRespirationsBronchial);
                 CheckBox RespiratoryRespirationsInspiratory = (CheckBox) findViewById(R.id.RespiratoryRespirationsInspiratory);
                 CheckBox RespiratoryRespirationsExpiratory = (CheckBox) findViewById(R.id.RespiratoryRespirationsExpiratory);
-
                 EditText RespiratoryRespirationsOther = (EditText) findViewById(R.id.RespiratoryRespirationsOther);
                 EditText RespiratoryTrachealStomaDescribe = (EditText) findViewById(R.id.RespiratoryTrachealStomaDescribe);
 
-                RespiratoryAirwayMaintainsOwn.setChecked(pa.isRespiratory_Airway_Maintains_Own());
-                BiPAPCPAP.setChecked(pa.isRespiratory_Airway_BIPAP_CPAP());
-                RespiratoryETT.setChecked(pa.isRespiratory_ETT());
-                RespiratorySize.setText(Float.toString(pa.getRespiratory_Size()));
-                RespiratoryLengthAtGums.setText(Float.toString(pa.getRespiratory_LengthAtGums()));// apparently setText only takes parameters int n shit.
-                RespiratoryLengthAtTeeth.setText(Float.toString(pa.getRespiratory_LengthAtTeeth()));
-                RespiratoryCuffPressure.setText(Float.toString(pa.getRespiratory_CuffPressure()));
-                RespiratoryTracheostomySize.setText(Float.toString(pa.getRespiratory_TracheostomySize()));
-                RespiratoryChestExpansion.setText(pa.getRespiratory_ChestExpansion());
-                RespiratoryCoughSimulatedBySuctioning.setChecked(pa.isRespiratory_Cough_SimulatedBySuctioning());
-                RespiratoryCoughSpontaneous.setChecked(pa.isRespiratory_Cough_Spontaneous());
-                RespiratoryCoughNotes.setText(pa.getRespiratory_Cough());
-
-                RespiratoryLipConditionIntact.setChecked(pa.isRespiratory_LipCondition_intact());
-                RespiratoryLipConditionOther.setChecked(pa.isRespiratory_LipCondition_other());
-                RespiratoryOralMucosaIntact.setChecked(pa.isRespiratory_OralMucosa_intact());
-                RespiratoryOralMucosaOther.setChecked(pa.isRespiratory_OralMucosa_other());
-
-                RespiratoryRespirationsClear.setChecked(pa.isRespiratory_Clear());
-                RespiratoryRespirationsDecreased.setChecked(pa.isRespiratory_Decreased());
-                RespiratoryRespirationsWheezes.setChecked(pa.isRespiratory_Wheezes());
-                RespiratoryRespirationsFineCreps.setChecked(pa.isRespiratory_FineCreps());
-                RespiratoryRespirationsAbsentBreath.setChecked(pa.isRespiratory_AbsentBreath());
-                RespiratoryRespirationsBronchial.setChecked(pa.isRespiratory_Bronchial());
-                RespiratoryRespirationsInspiratory.setChecked(pa.isRespiratory_Inspiratory());
-                RespiratoryRespirationsExpiratory.setChecked(pa.isRespiratory_Expiratory());
-
-                RespiratoryRespirationsOther.setText(pa.getRespiratory_OtherDescription());
-                RespiratoryTrachealStomaDescribe.setText(pa.getRespiratory_Trechea());
 
                 // Unsure about check boxes should work tho.
                 pa.setRespiratory_Airway_Maintains_Own(RespiratoryAirwayMaintainsOwn.isChecked());
                 pa.setRespiratory_Airway_BIPAP_CPAP(BiPAPCPAP.isChecked());
                 pa.setRespiratory_ETT(RespiratoryETT.isChecked());
-
                 // Really.. java.. thank god for conversion functions
                 pa.setRespiratory_Size(Float.valueOf(RespiratorySize.getText().toString()));
-                pa.setRespiratory_LengthAtGums(Float.valueOf(RespiratoryLengthAtGums.getText().toString()));
+                pa.setRespiratory_LengthAtGums(Float.valueOf(RespiratoryLengthAtGums.getText().toString())); // error here converting string to float. Is breaking the activity.
                 pa.setRespiratory_LengthAtTeeth(Float.valueOf(RespiratoryLengthAtTeeth.getText().toString()));
                 pa.setRespiratory_CuffPressure(Float.valueOf(RespiratoryCuffPressure.getText().toString()));
                 pa.setRespiratory_TracheostomySize(Float.valueOf(RespiratoryTracheostomySize.getText().toString()));
@@ -213,6 +181,20 @@ public class RespActivity extends Activity {
                 pa.setRespiratory_Cough_SimulatedBySuctioning(RespiratoryCoughSimulatedBySuctioning.isChecked());
                 pa.setRespiratory_Cough_Spontaneous(RespiratoryCoughSpontaneous.isChecked());
                 pa.setRespiratory_Cough(RespiratoryCoughNotes.getText().toString());
+                pa.setRespiratory_LipCondition_intact(RespiratoryLipConditionIntact.isChecked());
+                pa.setRespiratory_LipCondition_other(RespiratoryLipConditionOther.isChecked());
+                pa.setRespiratory_OralMucosa_intact(RespiratoryOralMucosaIntact.isChecked());
+                pa.setRespiratory_OralMucosa_other(RespiratoryOralMucosaOther.isChecked());
+                pa.setRespiratory_Clear(RespiratoryRespirationsClear.isChecked());
+                pa.setRespiratory_Decreased(RespiratoryRespirationsDecreased.isChecked());
+                pa.setRespiratory_Wheezes(RespiratoryRespirationsWheezes.isChecked());
+                pa.setRespiratory_FineCreps(RespiratoryRespirationsFineCreps.isChecked());
+                pa.setRespiratory_AbsentBreath(RespiratoryRespirationsAbsentBreath.isChecked());
+                pa.setRespiratory_Bronchial(RespiratoryRespirationsBronchial.isChecked());
+                pa.setRespiratory_Inspiratory(RespiratoryRespirationsInspiratory.isChecked());
+                pa.setRespiratory_Expiratory(RespiratoryRespirationsExpiratory.isChecked());
+                pa.setRespiratory_OtherDescription(RespiratoryRespirationsOther.getText().toString());
+                pa.setRespiratory_Trechea(RespiratoryTrachealStomaDescribe.getText().toString());
 
 
                 pa.update();
@@ -256,44 +238,14 @@ public class RespActivity extends Activity {
                 EditText RespiratoryRespirationsOther = (EditText) findViewById(R.id.RespiratoryRespirationsOther);
                 EditText RespiratoryTrachealStomaDescribe = (EditText) findViewById(R.id.RespiratoryTrachealStomaDescribe);
 
-                RespiratoryAirwayMaintainsOwn.setChecked(pa.isRespiratory_Airway_Maintains_Own());
-                BiPAPCPAP.setChecked(pa.isRespiratory_Airway_BIPAP_CPAP());
-                RespiratoryETT.setChecked(pa.isRespiratory_ETT());
-                RespiratorySize.setText(Float.toString(pa.getRespiratory_Size()));
-                RespiratoryLengthAtGums.setText(Float.toString(pa.getRespiratory_LengthAtGums()));// apparently setText only takes parameters int n shit.
-                RespiratoryLengthAtTeeth.setText(Float.toString(pa.getRespiratory_LengthAtTeeth()));
-                RespiratoryCuffPressure.setText(Float.toString(pa.getRespiratory_CuffPressure()));
-                RespiratoryTracheostomySize.setText(Float.toString(pa.getRespiratory_TracheostomySize()));
-                RespiratoryChestExpansion.setText(pa.getRespiratory_ChestExpansion());
-                RespiratoryCoughSimulatedBySuctioning.setChecked(pa.isRespiratory_Cough_SimulatedBySuctioning());
-                RespiratoryCoughSpontaneous.setChecked(pa.isRespiratory_Cough_Spontaneous());
-                RespiratoryCoughNotes.setText(pa.getRespiratory_Cough());
-
-                RespiratoryLipConditionIntact.setChecked(pa.isRespiratory_LipCondition_intact());
-                RespiratoryLipConditionOther.setChecked(pa.isRespiratory_LipCondition_other());
-                RespiratoryOralMucosaIntact.setChecked(pa.isRespiratory_OralMucosa_intact());
-                RespiratoryOralMucosaOther.setChecked(pa.isRespiratory_OralMucosa_other());
-
-                RespiratoryRespirationsClear.setChecked(pa.isRespiratory_Clear());
-                RespiratoryRespirationsDecreased.setChecked(pa.isRespiratory_Decreased());
-                RespiratoryRespirationsWheezes.setChecked(pa.isRespiratory_Wheezes());
-                RespiratoryRespirationsFineCreps.setChecked(pa.isRespiratory_FineCreps());
-                RespiratoryRespirationsAbsentBreath.setChecked(pa.isRespiratory_AbsentBreath());
-                RespiratoryRespirationsBronchial.setChecked(pa.isRespiratory_Bronchial());
-                RespiratoryRespirationsInspiratory.setChecked(pa.isRespiratory_Inspiratory());
-                RespiratoryRespirationsExpiratory.setChecked(pa.isRespiratory_Expiratory());
-
-                RespiratoryRespirationsOther.setText(pa.getRespiratory_OtherDescription());
-                RespiratoryTrachealStomaDescribe.setText(pa.getRespiratory_Trechea());
 
                 // Unsure about check boxes should work tho.
                 pa.setRespiratory_Airway_Maintains_Own(RespiratoryAirwayMaintainsOwn.isChecked());
                 pa.setRespiratory_Airway_BIPAP_CPAP(BiPAPCPAP.isChecked());
                 pa.setRespiratory_ETT(RespiratoryETT.isChecked());
-
                 // Really.. java.. thank god for conversion functions
                 pa.setRespiratory_Size(Float.valueOf(RespiratorySize.getText().toString()));
-                pa.setRespiratory_LengthAtGums(Float.valueOf(RespiratoryLengthAtGums.getText().toString()));
+                pa.setRespiratory_LengthAtGums(Float.valueOf(RespiratoryLengthAtGums.getText().toString())); // error here converting string to float. Is breaking the activity.
                 pa.setRespiratory_LengthAtTeeth(Float.valueOf(RespiratoryLengthAtTeeth.getText().toString()));
                 pa.setRespiratory_CuffPressure(Float.valueOf(RespiratoryCuffPressure.getText().toString()));
                 pa.setRespiratory_TracheostomySize(Float.valueOf(RespiratoryTracheostomySize.getText().toString()));
@@ -301,13 +253,24 @@ public class RespActivity extends Activity {
                 pa.setRespiratory_Cough_SimulatedBySuctioning(RespiratoryCoughSimulatedBySuctioning.isChecked());
                 pa.setRespiratory_Cough_Spontaneous(RespiratoryCoughSpontaneous.isChecked());
                 pa.setRespiratory_Cough(RespiratoryCoughNotes.getText().toString());
-
+                pa.setRespiratory_LipCondition_intact(RespiratoryLipConditionIntact.isChecked());
+                pa.setRespiratory_LipCondition_other(RespiratoryLipConditionOther.isChecked());
+                pa.setRespiratory_OralMucosa_intact(RespiratoryOralMucosaIntact.isChecked());
+                pa.setRespiratory_OralMucosa_other(RespiratoryOralMucosaOther.isChecked());
+                pa.setRespiratory_Clear(RespiratoryRespirationsClear.isChecked());
+                pa.setRespiratory_Decreased(RespiratoryRespirationsDecreased.isChecked());
+                pa.setRespiratory_Wheezes(RespiratoryRespirationsWheezes.isChecked());
+                pa.setRespiratory_FineCreps(RespiratoryRespirationsFineCreps.isChecked());
+                pa.setRespiratory_AbsentBreath(RespiratoryRespirationsAbsentBreath.isChecked());
+                pa.setRespiratory_Bronchial(RespiratoryRespirationsBronchial.isChecked());
+                pa.setRespiratory_Inspiratory(RespiratoryRespirationsInspiratory.isChecked());
+                pa.setRespiratory_Expiratory(RespiratoryRespirationsExpiratory.isChecked());
+                pa.setRespiratory_OtherDescription(RespiratoryRespirationsOther.getText().toString());
+                pa.setRespiratory_Trechea(RespiratoryTrachealStomaDescribe.getText().toString());
                 pa.update();
 
                 Intent intent = new Intent(RespActivity.this, CardioActivity.class);
                 startActivity(intent);
-
-
 
             }
         });
